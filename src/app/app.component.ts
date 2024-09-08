4af7b54aa64d52b916a83e6a6cd30b723d80036b
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-09-08 21:43:24 +0300

    app.changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subject, Observable, forkJoin, combineLatest } from 'rxjs';
+import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
+import { MockDataService } from './services/mock-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courses-app';
+  searchTermByCharacters = new Subject<string>();
+  characters$: Observable<string[]>;
+  planetAndCharactersResults$!: Observable<string[]>; 
+  isLoading = false;
+
+  constructor(private mockDataService: MockDataService) {
+
+    this.characters$ = this.searchTermByCharacters.pipe(
+      filter(term => term.length >= 3), 
+      debounceTime(300), 
+      switchMap(term => this.mockDataService.getCharacters(term)) 
+    );
+
+  
+    combineLatest([
+      this.mockDataService.getCharactersLoader(),
+      this.mockDataService.getPlanetLoader()
+    ]).pipe(
+      tap(([charLoader, planetLoader]) => {
+        this.isLoading = charLoader || planetLoader;
+      })
+    ).subscribe();
+  }
+
+  changeCharactersInput(term: string) {
+    this.searchTermByCharacters.next(term);
+  }
+
+  loadCharactersAndPlanets() {
+    this.planetAndCharactersResults$ = forkJoin([
+      this.mockDataService.getCharacters(''),
+      this.mockDataService.getPlanets()
+    ]).pipe(
+      tap(([characters, planets]) => {
+        console.log('Characters:', characters);
+        console.log('Planets:', planets);
+      })
+    );
+  }
 }
